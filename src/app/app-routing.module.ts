d6d1d958810b6504780d6e781a9d2eb26982eda0
Pablo <delatorre.pablodaniel@gmail.com>
2022-12-27 17:31:16 -0300

    guard agregado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { AppComponent } from './app.component';
 import { ExitoComponent } from './components/exito/exito.component';
 import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { GuardGuard } from './servicios/guard.guard';
 
 const routes: Routes = [
-  {path:'portfolio',component:PortfolioComponent},
+  {path:'portfolio',component:PortfolioComponent,canActivate:[GuardGuard]},
   {path:'exito',component:ExitoComponent},
   {path:'iniciar-sesion',component:IniciarSesionComponent},
   {path:'',redirectTo:'iniciar-sesion',pathMatch:'full'}
